4f5674bc8e0249dcc1a220d32bb679b0ac47b798
zuzuchka <tedokhaxutasishvili@gmail.com>
2024-07-01 19:34:56 +0400

    second commit

---
+++ day0/Untitled-1.py
@@ -0,0 +1,30 @@
+NAME="zura"
+#name არის ცვლადი
+#=ცვლადისთვის მიმნიჭებელი სიმბოლო
+#"zura" არის ცვლადისთვის მნიშვნელობა
+
+surname="qatamadze"
+
+#print(Name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name="zura"#ეს არის str (string) ტიპის ცვლადი
+
+age=11 # ეს არის int (integer) მთელი რიცხვი
+height=150 # ეს არის float ტიპის ცვლადი (ათასწლადი)
+#boolean(bool)ტიპის ცვლადი
+knous_programing=True #true ან false
+
+is_ungly= False
+isungy=False # ჯავასკრიფტული camelcase
+print(name+""+surname)
+#სტრინგი არის ბრჭყალებსი მოქცეული სიმბოლოები
+print(name+str(age))
+#print(tyep)(name)
+#print(tyep)(surname)
+#print(tyep)(haight)
+#print(tyep)(knows_(programing)
+
+print(name+ ""+str (age))
+
+
